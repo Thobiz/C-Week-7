ee9b23ee9b3613c28a1557d6b76fa7622217917c
Thobiz <69186131+Thobiz@users.noreply.github.com>
2020-08-25 11:53:03 +0700

    Add files via upload

---
+++ myC701.c
@@ -0,0 +1,16 @@
+/*
+Program : datatype
+By : Jirayoot Pandee
+Date: 8/25/63
+*/
+#include<stdio.h>
+void main(){
+    short a;
+    long b;
+    long c;
+    long double d;
+    printf("size of short =%d byte\n", sizeof(a));
+    printf("size of short =%d byte\n", sizeof(b));
+    printf("size of long long =%d byte\n", sizeof(c));
+    printf("size of long double =%d byte\n", sizeof(d));
+}
